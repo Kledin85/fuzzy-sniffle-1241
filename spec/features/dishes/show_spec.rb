b5690f9c8f8466170032458c86910c6efc0651b7
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-01-09 19:11:10 -0600

    have story 1 complere

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSPec.describe "show page" do
+RSpec.describe "show page" do
 
-  before each: do
+  before :each do
     @chef1 = Chef.create!(name: "name1")
-    @ingrediants1 = Ingredient.create!(name: "ingrediant1", calories: 1)
-    @ingrediants2 = Ingredient.create!(name: "ingrediant2", calories: 2)
-    @ingrediants3 = Ingredient.create!(name: "ingrediant3", calories: 3)
+    @ingrediant1 = Ingredient.create!(name: "ingrediant1", calories: 1)
+    @ingrediant2 = Ingredient.create!(name: "ingrediant2", calories: 2)
+    @ingrediant3 = Ingredient.create!(name: "ingrediant3", calories: 3)
     @dish1 = Dish.create!(name: "name1", description: "this is dish 1", chef_id: @chef1.id)
-    @dish1ingrediants = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingrediant1)
+    @dish1ingrediants = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingrediant1.id)
   end
   describe 'story 1' do 
 #     As a visitor
@@ -18,9 +18,21 @@ RSPec.describe "show page" do
 # and a total calorie count for that dish
 # And I see the chef's name.
     it 'shows the dishes name and descriptions' do
-      visit "dishes/#{@dish1}"
+      visit "dishes/#{@dish1.id}"
+      
+      expect(page).to have_content(@dish1.name)
+      expect(page).to have_content(@dish1.description)
     end
-    it 'shows a list of ingredients and total calorie count'
-    it 'shows the chefs name'
+    it 'shows a list of ingredients and total calorie count' do
+      visit "dishes/#{@dish1.id}"
+      save_and_open_page
+      expect(page).to have_content(@ingrediant1.name)
+      expect(page).to have_content(@ingrediant1.calories)
+    end
+    it 'shows the chefs name' do
+      visit "dishes/#{@dish1.id}"
+      
+      expect(page).to have_content(@dish1.chef.name)
+    end
   end
 end
